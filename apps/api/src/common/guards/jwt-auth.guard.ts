b8ded192163796f9606d3bdecbc8563c1ff29969
Joaomondardo <joaovitormondardodossantos@gmail.com>
2026-06-06 13:44:23 -0300

    docs: atualiza README completo + fix jwt-auth guard

--- apps/api/src/common/guards/jwt-auth.guard.ts
+++ apps/api/src/common/guards/jwt-auth.guard.ts
@@ -1,3 +1,4 @@
+import { Observable } from 'rxjs';
 import { Injectable, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
@@ -9,7 +10,7 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
     super();
   }
 
-  canActivate(context: ExecutionContext) {
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
     const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
       context.getHandler(),
       context.getClass(),
@@ -18,3 +19,4 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
     return super.canActivate(context);
   }
 }
+
